5aff793a682129c7a2124163e7aef25686f96c35
lukasj3731 <lukasj3731@gmail.com>
2020-12-04 09:22:34 +0100

    optimized a bit
    also stole an idea from a friend, code is even shorter now, thx

--- AOC2020/src/AOC04.java
+++ AOC2020/src/AOC04.java
@@ -5,27 +5,19 @@ public class AOC04 extends AOC{
 	}
 	
 	static void task1() {
-		String[] passports = StrInput("inputs/input04.txt").split("\n\n");
-		String[] neccessary = {"byr","iyr","eyr","hgt","hcl","ecl","pid"};
 		int valid = 0;
-		for(String p:passports) {
-			int c = 0;
-			for(String n:neccessary)
-				c += p.contains(n)?1:0;
-			valid += c==neccessary.length?1:0;
-		}
+		for(String p:StrInput("inputs/input04.txt").split("\n\n"))
+			valid += (p.split("[\n ]").length+(!p.contains("cid")?1:0)==8)?1:0;
 		println("Task 1: "+valid);
 	}
 	
 	static void task2() {
-		String[] passports = StrInput("inputs/input04.txt").split("\n\n");
-		String[] neccessary = {"byr","iyr","eyr","hgt","hcl","ecl","pid"};
 		int valid = 0;
-		for(String p:passports) {
-			int c = 0;
-			for(String n:neccessary)
-				c += isValid(p,n)?1:0;
-			valid += c==neccessary.length?1:0;
+		for(String p:StrInput("inputs/input04.txt").split("\n\n")) {
+			boolean c = true;
+			for(String n:p.split("[\n ]"))
+				c = isValid(p,regexFinder("([a-z]{3}):",n))?c:false;
+			valid += (c&&(p.split("[\n ]").length+(!p.contains("cid")?1:0)==8))?1:0;
 		}
 		println("Task 2: "+valid);
 	}
@@ -37,12 +29,7 @@ public class AOC04 extends AOC{
 		case "eyr": return regexBetween("eyr:([0-9]*)",p,2020,2030);
 		case "hgt": return regexBetween("hgt:([0-9]*)in",p,59,76) || regexBetween("hgt:([0-9]*)cm",p,150,193);
 		case "hcl": return regexFinder("hcl:(#[a-f0-9]*)",p).length()==7;
-		case "ecl": 
-			boolean ret = false;
-			String[] col = {"amb","blu","brn","gry","grn","hzl","oth"};
-			for(String c:col)
-				ret = c.equals(regexFinder("ecl:([a-z]*)",p))?true:ret;
-			return ret;
+		case "ecl": return regexFinder("ecl:(amb|blu|brn|gry|grn|hzl|oth)",p).length()>0;
 		default: return regexFinder("pid:([0-9]*)",p).length()==9; //pid
 		}
 	}
