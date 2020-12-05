0724eec67691d211f0a12df56233f7aa0d856443
lukasj3731 <lukasj3731@gmail.com>
2020-12-05 06:44:14 +0100

    added comments

--- AOC2020/src/AOC05.java
+++ AOC2020/src/AOC05.java
@@ -8,9 +8,9 @@ public class AOC05 extends AOC{
 	}
 
 	static void task1() {
-		int max = -1;
+		int max = 0;
 		for(String s:StringArrInput("inputs/input05.txt"))
-			max = Math.max(getBin(s.substring(0, 7),'B')*8+getBin(s.substring(7, 10),'R'), max);	
+			max = Math.max(getBin(s.substring(0, 7),'B')*8+getBin(s.substring(7, 10),'R'),max);	
 		println("Task 1: "+max);
 	}
 
@@ -18,12 +18,12 @@ public class AOC05 extends AOC{
 		List<Integer> l = new LinkedList<Integer>();
 		for(String s:StringArrInput("inputs/input05.txt"))
 			l.add(getBin(s.substring(0, 7),'B')*8+getBin(s.substring(7, 10),'R'));
-		int i=0;
-		for(;!(l.contains(i-1)&&l.contains(i+1)&&!l.contains(i));i++) {}
-		println("Task 2: "+i);
+		int id=0;
+		for(;!(l.contains(id-1)&&l.contains(id+1)&&!l.contains(id))&&id<9999;id++) {}	//the id<9999 is unneccessary, but i feel safer that way
+		println("Task 2: "+id);
 	}
 
-	static int getBin(String in, char one) {
+	static int getBin(String in, char one) {	//turns a string of symbols into binary, where <one> indicates a 1 and any other symbol a 0
 		int id=0;
 		for(int i=0;i<in.length();i++)
 			id=2*id+(in.charAt(i)==one?1:0);
