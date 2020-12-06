3c056a7232949fd91529e36f3257aaf66203a539
lukasj3731 <lukasj3731@gmail.com>
2020-12-06 18:33:43 +0100

    oh my god I'm sorry for this code

--- AOC2020/src/AOC06.java
+++ AOC2020/src/AOC06.java
@@ -1,26 +1,17 @@
-public class AOC06 extends AOC{
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class AOC06 extends AOC{	//wanted to see if i can do today in one-liners
 	public static void main(String[] args) {
 		task1();
 		task2();
 	}
 	
 	static void task1() {
-		int sum = 0;
-		for(String answers:StrInput("inputs/input06.txt").split("\n\n"))
-			for(char c='a';c<='z';c++)
-				sum += answers.contains(c+"")?1:0;
-		println("Task 1: "+sum);
+		println("Task 1: "+Arrays.stream(StrInput("inputs/input06.txt").split("\n\n")).mapToLong( /*for every group*/ (answers -> (Arrays.stream(answers.replaceAll("\n","").split("")).distinct().count()))).sum());	//count distinct answers and sum
 	}
 	
 	static void task2() {
-		int sum = 0;
-		for(String answers:StrInput("inputs/input06.txt").split("\n\n"))
-			for(char c='a';c<='z';c++) {
-				int ppl = 0;
-				for(String s:answers.split("\n")) 
-					ppl += s.contains(c+"")?1:0;
-				sum += (ppl==answers.split("\n").length)?1:0;
-			}
-		println("Task 1: "+sum);
+		println("Task 2: "+Arrays.stream(StrInput("inputs/input06.txt").split("\n\n")).mapToLong( /*for every group*/ answers -> IntStream.range((int)'a',((int)'z')+1).map( /*for every char*/ c -> (Arrays.stream(answers.split("\n"))).mapToInt(s -> s.contains(""+(char)c)?1:0).sum()).map( /*sum occurence of char*/ i -> (i==answers.split("\n").length?1:0)).sum()).sum());	//and save 1 if every person answered, then sum it all up
 	}
 }
