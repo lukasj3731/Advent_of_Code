581268282afc4ec224f243581baa2de031530b4b
lukasj3731 <lukasj3731@gmail.com>
2020-12-07 09:06:21 +0100

    commented/optimized a bit
    also tried a recursive approach. got it working but took wayyy longer than this, so this iterative code is staying.

--- AOC2020/src/AOC07.java
+++ AOC2020/src/AOC07.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class AOC07 extends AOC{
+public class AOC07 extends AOC {
 	public static void main(String[] args) {
 		task1();
 		task2();
@@ -10,48 +10,46 @@ public class AOC07 extends AOC{
 	static void task1() {
 		String[] in = StringArrInput("inputs/input07.txt");
 		Map<String, Boolean> m = new HashMap<String, Boolean>();
-		for(int i=0;i<in.length;i++)
-			if(in[i].contains("shiny gold bags contain")) 
-				m.put(regexFinder("([a-z]* [a-z]*) ", in[i]),true);
+		for (int i = 0; i < in.length; i++)	//add shiny gold bag to map
+			if (in[i].contains("shiny gold bags contain"))
+				m.put(regexFinder("([a-z]* [a-z]*) ", in[i]), true);
 		int curr = 0;
-		int prev = -1;
-		while(prev != curr) {
-			for(int i=0;i<in.length;i++) {
-				boolean found = false;
-				for(String s:m.keySet())
-					found = in[i].contains(s)?true:found;
-				if (found)
-					m.put(regexFinder("([a-z]* [a-z]*) ", in[i]),true);
-			}
-			prev = curr;
+		do {				//while there are still bags being added
 			curr = m.size();
-		}
-		println("Task 1: "+(curr-1));
+			for (int i = 0; i < in.length; i++) {	//for every bag
+				if(m.containsKey(regexFinder("([a-z]* [a-z]*)", in[i]))) {continue;}	//skip if already in map
+				boolean found = false;
+				for (String s : m.keySet())	//for every existing bag
+					found = in[i].contains(s) ? true : found; //if existing bag in list, found = true
+				if (found)
+					m.put(regexFinder("([a-z]* [a-z]*) ", in[i]), true);	//add found bag
+			}
+		} while (curr < m.size());
+		println("Task 1: " + (curr - 1));	//-1 cause shiny gold itself doesn't count
 	}
-	
+
 	static void task2() {
 		String[] in = StringArrInput("inputs/input07.txt");
 		Map<String, Integer> m = new HashMap<String, Integer>();
-		for(int i=0;i<in.length;i++)
-			if(in[i].contains(" no other "))
-				m.put(regexFinder("([a-z]* [a-z]*) ", in[i]),0);
+		for (int i = 0; i < in.length; i++)	//save all empty bags with 0 contents in map
+			if (in[i].contains(" no other "))
+				m.put(regexFinder("([a-z]* [a-z]*) ", in[i]), 0);
 		int curr = 0;
-		int prev = -1;
-		while(prev != curr) {
-			for(int i=0;i<in.length;i++) {
-				boolean allComponents = true;
-				int sum = 0;
-				for(String s:in[i].split("contain")[1].split(",")) {
-					allComponents = m.containsKey(regexFinder(" ([a-z]* [a-z]*)",s))?allComponents:false;
-					if(allComponents)
-						sum += Integer.parseInt(regexFinder(" ([0-9]*)",s)) * (1+ m.get(regexFinder(" ([a-z]* [a-z]*)",s)));
-				}
-				if(allComponents)
-					m.put(regexFinder("([a-z]* [a-z]*) ", in[i]),sum);
-			}
-			prev = curr;
+		do {	//while there are still bags being added
 			curr = m.size();
-		}
-		println("Task 2: "+m.get("shiny gold"));
+			for (int i = 0; i < in.length; i++) {	//for every bag
+				if(m.containsKey(regexFinder("([a-z]* [a-z]*)", in[i]))) {continue;}	//skip if already in map
+				boolean allComponents = true;	
+				int sum = 0;
+				for (String s : in[i].split("contain")[1].split(",")) {
+					allComponents = m.containsKey(regexFinder(" ([a-z]* [a-z]*)", s)) ? allComponents : false;	//see if every component exists in map
+					sum += allComponents	//build sum of component bags
+							? pInt(regexFinder(" ([0-9]*)", s)) * (1 + m.get(regexFinder(" ([a-z]* [a-z]*)", s))) : 0;
+				}
+				if (allComponents)	//if all components in map, aff current bag
+					m.put(regexFinder("([a-z]* [a-z]*) ", in[i]), sum);
+			}
+		} while (curr < m.size());
+		println("Task 2: " + m.get("shiny gold"));
 	}
 }
