9a61e73819d3a166229abcb88301c61b4e72e4ad
lukasj3731 <lukasj3731@gmail.com>
2020-12-13 17:25:03 +0100

    look what daniel made me do

--- AOC2020/src/AOC10.java
+++ AOC2020/src/AOC10.java
@@ -19,18 +19,18 @@ public class AOC10 extends AOC{
 		println("Task 1: "+(diffs[0]*diffs[1]));
 	}
 
-	static void task2() {
-		List<Integer> l = intListInput("inputs/input10.txt");
-		l.add(0);
-		l.sort(null);
-		l.add(l.get(l.size()-1)+3);
-		Map<Integer, Long> m = new HashMap<Integer, Long>();
-		for(int i=l.size()-1;i>=0;i--) {	//moving backwards through list
-			long total = i==l.size()-1?1:0;	//biggest element starts with 1, others with 0
-			for(int j=1;j<=3;j++)	//for every possible adapter
-				total += (m.containsKey(l.get(i)+j))?m.get(l.get(i)+j):0;	//add possibilities to total
-			m.put(l.get(i), total);	//add total to map
-		}
-		println("Task 2: "+m.get(0));
+	static void task2() {		//got a complaint my code isn't very understandable. daniel, this excessive commenting is your fault
+		List<Integer> l = intListInput("inputs/input10.txt");	//reading the input file into a list called l
+		l.add(0);	//adding 0 to this list. this element is equivalent to the wall outlet
+		l.sort(null);	//sorting the list so that the smallest element is first and the biggest element is last
+		l.add(l.get(l.size()-1)+3);	//adding the laptop to the list. the laptop is 3 jolts over the biggest (aka the last) element in the list
+		Map<Integer, Long> m = new HashMap<Integer, Long>();	//initializing a list. it maps from a chargers jolts to the possibilities of plugs after it. e.g.: [10->5962] means there is 5962 ways to plug the laptop into a charger with 10 jolts
+		for(int i=l.size()-1;i>=0;i--) {	//moving backwards through list, starting with the laptop
+			long total = i==l.size()-1?1:0;	//the laptop is initialized with one possibility, all other elements start with 0. thats cause the laptop has nothing it has to be plugged into
+			for(int j=1;j<=3;j++)	//iterating through every reachable adapter from the current one (those are adapters with jolts+1, jolts+2 and jolts+3)
+				total += (m.containsKey(l.get(i)+j))?m.get(l.get(i)+j):0;	//if such an adapter exists, the possible ways to plug it in are added to the total.
+			m.put(l.get(i), total);	//the total is then added to the map, so that future adapters can use it's value for calculation
+		}	//here I'm closing a bracket
+		println("Task 2: "+m.get(0));	//lastly, we print the possibilities to plug into the outlet. this number equals all possible charger arrangements between outlet and laptop
 	}
 }
