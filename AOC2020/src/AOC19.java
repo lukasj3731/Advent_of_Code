db47ddb5e18ddd827d0d18ef1f66f144cf7dc5be
lukasj3731 <lukasj3731@gmail.com>
2020-12-19 09:06:21 +0100

    optimized day 19 and added comments

--- AOC2020/src/AOC19.java
+++ AOC2020/src/AOC19.java
@@ -5,79 +5,44 @@ public class AOC19 extends AOC{
 		task1();
 		task2();
 	}
-
+	
 	static void task1() {
-		String in = StrInput("inputs/input19.txt");
-
-		String[] rules = in.split("\n\n")[0].split("\n");
-		String[] lines = in.split("\n\n")[1].split("\n");
-		HashMap<Integer, String> m = new HashMap<Integer,String>();
-
-		for(String line: rules) {
-			m.put(pInt(line.split(": ")[0]),line.split(": ")[1]);
-		}
-		String r = getRegex(0,m, false);
-		int sum = 0;
-		for(String l:lines)
-			sum += l.matches(r)?1:0;
-		println("Task 1: "+sum);
+		task(false);
 	}
-
-	
-	
-	static String getRegex(int rule, HashMap<Integer, String> m, boolean t2) {
-		
-		if(rule == 8 && t2) {
-			return "("+getRegex(42, m, t2)+"+)";
-		} else if(rule == 11 && t2) {
-        	String r42 = getRegex(42,m,t2);
-        	String r31 = getRegex(31,m,t2);
-        	
-        	String ret = "(";
-        	for(int i=1;i<10;i++) {
-        		ret += "("+r42+"{"+i+"}"+r31+"{"+i+"})|";
-        	}
-        	ret = ret.substring(0, ret.length()-1)+")";
-        	return ret;
-        }
-		
-		String r = m.get(rule);
-		if(r.contains("\"")) {
-			return r.replaceAll("\"", "");
-		}
-		
-		String ret = "(";
-		for(String s:r.split(" ") ){
-			if(s.equals("|")) {
-				ret += "|";
-			} else {
-				ret += getRegex(pInt(s),m, t2);
-			}
-		}
-		return ret +")";
-	}
-	
-	
 	
 	static void task2() {
-		String in = StrInput("inputs/input19.txt");
-
-		String[] rules = in.split("\n\n")[0].split("\n");
-		String[] lines = in.split("\n\n")[1].split("\n");
-		HashMap<Integer, String> m = new HashMap<Integer,String>();
-
-		for(String line: rules) {
-			m.put(pInt(line.split(": ")[0]),line.split(": ")[1]);
-		}
-		String r = getRegex(0,m, true);
-		int sum = 0;
-		for(int i=0;i<lines.length;i++) {
-			String test = lines[i];
-			//test = m.get(0).match(test,m, test.length());
-			if(test.matches(r)) {
-				sum++;
-			}
-		}
-		println("Task 1: "+sum);
+		task(true);
 	}
+	
+	static void task(boolean t2) {
+		String in = StrInput("inputs/input19.txt");
+		HashMap<Integer, String> m = new HashMap<Integer,String>();	//maps [ruleNumber -> ruleSet]
+		for(String rule: in.split("\n\n")[0].split("\n"))
+			m.put(pInt(rule.split(":")[0]),rule.split(": ")[1]);	//add every rule to map
+		String r = getRegex(0,m,t2);	//generate regex for ruleset
+		int sum = 0;
+		for(String line:in.split("\n\n")[1].split("\n"))	//increment sum if line matches regex
+			sum += line.matches(r)?1:0;
+		println("Task "+(t2?2:1)+": "+sum);
+	}
+
+	static String getRegex(int rule, HashMap<Integer, String> m, boolean t2) {
+		if(rule == 8 && t2)	//for task 2: rule 8 is (rule42)+
+			return "("+getRegex(42, m, t2)+"+)";
+		if(rule == 11 && t2) {	//for task 2: rule 11 is (rule42{n} rule31{n}) where n is an integer between 1 and 20 (20 is just an assumption, I hope this always works?)
+        	String c1 = getRegex(42,m,t2);
+        	String c2 = getRegex(31,m,t2);
+        	String ret = "(";
+        	for(int i=1;i<20;i++)
+        		ret += "("+c1+"{"+i+"}"+c2+"{"+i+"})|";
+        	return ret.substring(0, ret.length()-1)+")";
+        }
+		String r = m.get(rule);		//for every other rule
+		if(r.contains("\""))	//if it contains ", it's a terminator, so 'a' or 'b'
+			return r.replaceAll("\"", "");
+		String ret = "(";
+		for(String s:r.split(" ") )	//otherwise add 'or' separated regexes
+			ret += s.equals("|")?"|":getRegex(pInt(s),m, t2);
+		return ret +")";
+	}	//i could use a map for memorizing regexes but the regex generation is not taking long compared to the evaluation so I didn't bother
 }
