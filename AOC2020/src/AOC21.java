0c67ef4e73616f299103ea85d18a8790cfaeae2d
lukasj3731 <lukasj3731@gmail.com>
2020-12-21 09:36:44 +0100

    optimized and commented day 21

--- AOC2020/src/AOC21.java
+++ AOC2020/src/AOC21.java
@@ -2,114 +2,63 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 
+import javafx.scene.AmbientLight;
+
 public class AOC21 extends AOC {
 	public static void main(String[] args) {
-		task1();
-		task2();
-		
+		println("Task 1: "+task1());
+		println("Task 2: "+task2());
 	}
 	
-	static void task1() {
-		String[] in = StringArrInput("inputs/input21.txt");
+	static int task1() {
 		HashMap<String, HashSet<String>> m = new HashMap<String, HashSet<String>>();
 		HashMap<String, Integer> total = new HashMap<String, Integer>();
-		
-		for(String l: in) {
-			String[] ingr = l.split("\\(")[0].split(" ");
-			String[] allergens = regexFinder("\\(([^()]+)\\)", l).replaceAll("contains ", "").split(", ");
-			HashSet<String> list = new HashSet<String>();
-			for(String s:ingr) {
-				list.add(s);
-				total.put(s, total.getOrDefault(s, 0)+1);
-			}
-			
-			for(String a:allergens) {
-				if(m.containsKey(a)) {
-					HashSet<String> list2 = m.get(a);
-					HashSet<String> ret = new HashSet<String>();
-					for(String s:list2) {
-						if(list.contains(s)) {
-							ret.add(s);
-						}
-					}
-					m.put(a, ret);
-				} else {
-					m.put(a, list);
-				}
-			}
-		}
-		
-		for(String s:m.keySet()) {
-			for(String i:m.get(s)) {
-				total.remove(i);
-			}
-		}
-		
+		fillMaps(m,total);
+		for(String s:m.keySet())
+			for(String i:m.get(s))
+				total.remove(i);	//remove every element that could be an ingredíent
 		int sum = 0;
-		
-		for(String s:total.keySet()) {
-			sum += total.get(s);
-		}
-		
-		
-		
-		
-		println("Task 1: "+sum);
+		for(String s:total.keySet())
+			sum += total.get(s);	//sum leftover elements
+		return sum;
 	}
 	
-	static void task2() {
-		String[] in = StringArrInput("inputs/input21.txt");
+	static String task2() {
 		HashMap<String, HashSet<String>> m = new HashMap<String, HashSet<String>>();
 		HashMap<String, Integer> total = new HashMap<String, Integer>();
-		
-		for(String l: in) {
-			String[] ingr = l.split("\\(")[0].split(" ");
-			String[] allergens = regexFinder("\\(([^()]+)\\)", l).replaceAll("contains ", "").split(", ");
+		fillMaps(m,total);
+		LinkedList<String> ingr = new LinkedList<String>();
+		for(String safe = ".";!safe.equals("");) {	//loop to remove duplicates until only identifiable ingredients are left
+			safe = "";
+			for(String s:m.keySet())
+				if(m.get(s).size()==1)
+					safe = s+":"+m.get(s).iterator().next();
+			if(!safe.equals("")) {
+				for(String s:m.keySet())
+					m.get(s).remove(safe.split(":")[1]);
+				ingr.add(safe);
+			}
+		}
+		ingr.sort(null);	//sort ingredients
+		return ingr.toString().replaceAll("\\[|\\]| ", "").replaceAll("([a-z]*:)","");	//remove actual ingredient name and only keep obscured name
+	}
+	
+	static void fillMaps(HashMap<String, HashSet<String>> m ,HashMap<String, Integer> total) {	//fills map m: [allergene -> possible ingredients], total: [ingredient -> occurrence]
+		for(String l: StringArrInput("inputs/input21.txt")) {
 			HashSet<String> list = new HashSet<String>();
-			for(String s:ingr) {
+			for(String s:l.split("\\(")[0].split(" ")) {	//ingredients
 				list.add(s);
 				total.put(s, total.getOrDefault(s, 0)+1);
 			}
-			
-			for(String a:allergens) {
+			for(String a:regexFinder("\\(([^()]+)\\)", l).replaceAll("contains ", "").split(", ")) {	//allergens
 				if(m.containsKey(a)) {
-					HashSet<String> list2 = m.get(a);
-					HashSet<String> ret = new HashSet<String>();
-					for(String s:list2) {
-						if(list.contains(s)) {
-							ret.add(s);
-						}
-					}
-					m.put(a, ret);
+					HashSet<String> tmp = new HashSet<String>(m.get(a));
+					tmp.retainAll(list);
+					m.put(a, tmp);
 				} else {
 					m.put(a, list);
 				}
 			}
 		}
-		
-		
-		
-		LinkedList<String> ingr = new LinkedList<String>();
-		for(int i=0;i<100;i++) {
-			String safe = "";
-			String name = "";
-			for(String s:m.keySet()) {
-				if(m.get(s).size()==1) {
-					safe = m.get(s).iterator().next();
-					name = s;
-				}
-			}
-			for(String s:m.keySet()) {
-				m.get(s).remove(safe);
-			}
-			if(!safe.equals(""))
-				ingr.add(name+":"+safe);
-		}
-		
-	
-		ingr.sort(null);
-		
-		
-		println("Task 2: "+ingr.toString().replaceAll("\\[|\\]| ", "").replaceAll("([a-z]*:)",""));
 	}
 }
