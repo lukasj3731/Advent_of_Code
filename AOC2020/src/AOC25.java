e9f64745893ee7ddb46c2e08fb234f7c5c293f94
lukasj3731 <lukasj3731@gmail.com>
2020-12-25 07:56:25 +0100

    minor adjustement to save one loop

--- AOC2020/src/AOC25.java
+++ AOC2020/src/AOC25.java
@@ -1,18 +1,17 @@
 public class AOC25 extends AOC {
 	public static void main(String[] args) {
-		 println("Task 1: "+task1());
-		 println("Task 2: "+task2());
+		println("Task 1: "+task1());
+		println("Task 2: "+task2());
 	}
 
 	static long task1() {
 		int[] in = intArrInput("inputs/input25.txt"); // read public keys
-		long loop,val = 1;
-		for(loop = 0;in[0] != val;loop++)	//calculate loop size by iterating until public key is reached
-			val = (val * 7)%20201227;
-		val = 1;	//reset val to 1
-		for (int i = 0; i < loop; i++)	//calculate the key by transforming the doors public key with the calculated loop size
-			val = (val * in[1])% 20201227;
-		return val;
+		long val = 1, key = 1;
+		while(in[0] != val) {	//get loop size by iterating until cards public key is reached, iterate doors public key simultaneously
+			val = val*7%20201227;
+			key = key*in[1]%20201227;
+		}
+		return key;
 	}
 
 	static String task2() {
