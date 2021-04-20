966b13d340ffd9519834aa6e08f2cabebebae946
lukasj3731 <lukasj3731@gmail.com>
2021-04-20 09:46:08 +0200

    now both tasks complete!

--- AOC2017/day16.py
+++ AOC2017/day16.py
@@ -1,23 +1,34 @@
 def solve16():
     programs = 'abcdefghijklmnop'
     instructions = open('inputs/day16.txt').read().split(',')
-    for instr in instructions:
-        programs = do_op(programs, instr)
-    print('Task 1:',programs)
+    seen = dict()
+    i = 1
+    while True:
+        for instr in instructions:
+            programs = do_op(programs, instr)
+        if programs in seen.keys():
+            break
+        else:
+            seen[programs] = i
+            i += 1
+    loop = i - seen[programs]
+    iterations = 1000000000 % loop
+    programs = 'abcdefghijklmnop'
+    for i in range(iterations + loop):
+        for instr in instructions:
+            programs = do_op(programs, instr)
+        if i == 0:
+            print('Task 1:', programs)
+    print('Task 2:', programs)
 
 
 def do_op(programs, instr):
     if instr[0] == 's':
         value = int(instr[1:])
-        programs = programs[len(programs)-value:] + programs[:len(programs)-value]
-    elif instr[0] == 'x':
-        characters = [programs[int(x)] for x in instr[1:].split('/')]
+        return programs[len(programs)-value:] + programs[:len(programs)-value]
+    else:
+        characters = [programs[int(x)] for x in instr[1:].split('/')] if instr[0] == 'x' else instr[1:].split('/')
         programs = programs.replace(characters[0], '_')
         programs = programs.replace(characters[1], characters[0])
         programs = programs.replace('_', characters[1])
-    elif instr[0] == 'p':
-        characters = instr[1:].split('/')
-        programs = programs.replace(characters[0], '_')
-        programs = programs.replace(characters[1], characters[0])
-        programs = programs.replace('_', characters[1])
-    return programs
+        return programs
