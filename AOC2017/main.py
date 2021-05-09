87c09f1ceeb56f8d1b05bcdff7d5e2211b621445
lukasj3731 <lukasj3731@gmail.com>
2021-05-09 23:56:36 +0200

    takes ages, will (maybe) optimize later, when done with part 2

--- AOC2017/main.py
+++ AOC2017/main.py
@@ -13,6 +13,15 @@ from day12 import solve12
 from day13 import solve13
 from day14 import solve14
 from day15 import solve15
+from day16 import solve16
+from day17 import solve17
+from day18 import solve18
+from day19 import solve19
+from day20 import solve20
+from day21 import solve21
+from day22 import solve22
+from day23 import solve23
+from day24 import solve24
 
 if __name__ == '__main__':
-    eval('solve15()')
+    eval('solve24()')
